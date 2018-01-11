6df17c71f744799c8c443300802ec7c0c3772a21
esteban.duran <esteban.duran@pragma.com.co>
2018-01-11 17:43:17 -0500

    HeroService added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', birthday: new Date('1988, 3, 15') },
+  { id: 12, name: 'Narco', birthday: new Date('1988, 3, 20') },
+  { id: 13, name: 'Bombasto', birthday: new Date('1988, 4, 15') },
+  { id: 14, name: 'Celeritas', birthday: new Date('1988, 10, 15') },
+  { id: 15, name: 'Magneta', birthday: new Date('1900, 3, 21') },
+  { id: 16, name: 'RubberMan', birthday: new Date('1988, 3, 15') },
+  { id: 17, name: 'Dynama', birthday: new Date('1988, 3, 15') },
+  { id: 18, name: 'Dr IQ', birthday: new Date('1988, 3, 15') },
+  { id: 19, name: 'Magma', birthday: new Date('1988, 3, 15') },
+  { id: 20, name: 'Tornado', birthday: new Date('1988, 3, 15') }
+];
